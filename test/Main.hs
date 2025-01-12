0bcceead22c1a2c48981507866dfd55fc6538089
Ahmed Hagi <ahmedosman@mac.com>
2025-01-12 11:25:25 +0000

    Re-structure project

--- test/Main.hs
+++ test/Main.hs
@@ -9,7 +9,7 @@ import Text.XML.Cursor
 import Database.SQLite.Simple
 import System.Directory (removeFile, doesFileExist)
 import Control.Exception (bracket)
-import Control.Monad ((>=>))
+import Control.Monad (void)
 import qualified Database as DB
 import XmlParser
 import XmlTypes
@@ -21,6 +21,7 @@ setupTestDb = do
     execute_ conn "CREATE TABLE olcparam (xml_name TEXT, m_e TEXT)"
     execute_ conn "CREATE TABLE test_table (xml_param_name TEXT, xml_value TEXT, eplan_value TEXT)"
     
+    -- Add test data
     execute conn "INSERT INTO olcparam (xml_name, m_e) VALUES (?, ?)" 
            ("ETOCONVPS_01" :: T.Text, "m" :: T.Text)
     execute conn "INSERT INTO olcparam (xml_name, m_e) VALUES (?, ?)" 
@@ -31,7 +32,7 @@ setupTestDb = do
     
     return conn
 
--- Sample XML content for testing
+-- Test XML content
 sampleXml :: TL.Text
 sampleXml = TL.fromStrict $ T.unlines
     [ "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
@@ -44,107 +45,117 @@ sampleXml = TL.fromStrict $ T.unlines
     , "</ILOGICEPLAN>"
     ]
 
--- Helper function to parse XML safely
-parseTestXml :: TL.Text -> IO XML.Document
-parseTestXml input = do
-    case XML.parseText XML.def input of
-        Left err -> fail $ "Failed to parse XML: " ++ show err
-        Right doc -> return doc
+-- Helper function to parse test XML
+parseTestXml :: TL.Text -> Either String XML.Document
+parseTestXml input = case XML.parseText XML.def input of
+    Left err -> Left $ show err
+    Right doc -> Right doc
 
 main :: IO ()
 main = hspec $ do
     describe "Database Operations" $ do
-        it "should correctly identify mechanical parameters" $ do
+        it "identifies mechanical parameters correctly" $ do
             bracket setupTestDb close $ \conn -> do
                 result <- DB.checkMechanicalParam conn "ETOCONVPS_01"
                 result `shouldBe` True
+                
+                result2 <- DB.checkMechanicalParam conn "NONEXISTENT"
+                result2 `shouldBe` False
 
-        it "should correctly identify electrical parameters" $ do
+        it "identifies electrical parameters correctly" $ do
             bracket setupTestDb close $ \conn -> do
                 result <- DB.checkElectricalParam conn "ETOPRDH_01"
                 result `shouldBe` True
+                
+                result2 <- DB.checkElectricalParam conn "NONEXISTENT"
+                result2 `shouldBe` False
 
-        it "should return correct DB query results" $ do
+        it "returns correct database query results" $ do
             bracket setupTestDb close $ \conn -> do
                 result <- DB.queryDb conn "test_table" "ETOPRDH_01" "5"
                 result `shouldBe` Just "Low product height"
+                
+                result2 <- DB.queryDb conn "test_table" "NONEXISTENT" "5"
+                result2 `shouldBe` Just "nope"
 
     describe "XML Parsing" $ do
-        it "should parse due diligence parameters correctly" $ do
-            doc <- parseTestXml sampleXml
-            let ddMap = parseDueDiligence doc
-            Map.lookup "ETOCONVPS_01" ddMap `shouldBe` Just "PS_Value"
+        it "parses valid XML successfully" $ do
+            case parseTestXml sampleXml of
+                Left err -> expectationFailure $ "Failed to parse valid XML: " ++ err
+                Right _ -> return ()
 
-        it "should parse product heights correctly" $ do
-            doc <- parseTestXml sampleXml
-            let heights = parseProductHeights doc
-            heights `shouldBe` ["5", "10"]
+        it "parses due diligence parameters correctly" $ do
+            case parseTestXml sampleXml of
+                Left err -> expectationFailure err
+                Right doc -> do
+                    let ddMap = parseDueDiligence doc
+                    Map.lookup "ETOCONVPS_01" ddMap `shouldBe` Just "PS_Value"
+                    Map.lookup "NONEXISTENT" ddMap `shouldBe` Nothing
 
-        it "should parse table type correctly" $ do
-            doc <- parseTestXml sampleXml
-            let tabletype = parseTableType doc
-            tabletype `shouldBe` "test_table"
+        it "parses product heights correctly" $ do
+            case parseTestXml sampleXml of
+                Left err -> expectationFailure err
+                Right doc -> do
+                    let heights = parseProductHeights doc
+                    heights `shouldBe` ["5", "10"]
 
-        it "should parse order dictionary correctly" $ do
-            doc <- parseTestXml sampleXml
-            let orderDict' = parseOrderDict doc defaultDDOptions
-            Map.lookup "ETOSPID_01" orderDict' `shouldBe` Just "Order1"
+        it "handles missing product heights gracefully" $ do
+            let xmlWithoutHeights = "<?xml version=\"1.0\"?><ILOGICEPLAN><OTHER>value</OTHER></ILOGICEPLAN>"
+            case parseTestXml (TL.fromStrict xmlWithoutHeights) of
+                Left err -> expectationFailure err
+                Right doc -> do
+                    let heights = parseProductHeights doc
+                    heights `shouldBe` []
 
     describe "XML Generation" $ do
-        it "should generate mechanical XML correctly" $ do
-            doc <- parseTestXml sampleXml
-            let xmlInterp = XmlInterp 
-                    { xmlRoot = doc
-                    , dueDiligence = parseDueDiligence doc
-                    , productHeights = parseProductHeights doc
-                    , orderDict = parseOrderDict doc defaultDDOptions
-                    , tableType = parseTableType doc
-                    }
-            
-            let mechDoc = createMechanicalXml xmlInterp
-            let cursor = fromDocument mechDoc
-            let content' = cursor $// element "ETOCONVPS_01" &/ content
-            content' `shouldBe` ["PS_Value"]
+        it "generates mechanical XML with correct structure" $ do
+            case parseTestXml sampleXml of
+                Left err -> expectationFailure err
+                Right doc -> do
+                    let xmlInterp = XmlInterp 
+                            { xmlRoot = doc
+                            , dueDiligence = parseDueDiligence doc
+                            , productHeights = parseProductHeights doc
+                            , orderDict = parseOrderDict doc defaultDDOptions
+                            , tableType = parseTableType doc
+                            }
+                    
+                    let mechDoc = createMechanicalXml xmlInterp
+                    let cursor = fromDocument mechDoc
+                    let ps = cursor $// element "ETOCONVPS_01" &/ content
+                    ps `shouldBe` ["PS_Value"]
 
-        it "should generate electrical XML correctly" $ do
+        it "generates electrical XML with correct structure" $ do
             bracket setupTestDb close $ \conn -> do
-                doc <- parseTestXml sampleXml
-                let xmlInterp = XmlInterp 
-                        { xmlRoot = doc
-                        , dueDiligence = parseDueDiligence doc
-                        , productHeights = parseProductHeights doc
-                        , orderDict = parseOrderDict doc defaultDDOptions
-                        , tableType = parseTableType doc
-                        }
-                
-                elecDoc <- createElectricalXml xmlInterp conn
-                let cursor = fromDocument elecDoc
-                let configVars = cursor $// element "ConfigurationVariable"
-                length configVars `shouldBe` 1
-                
-                let heightVar = head configVars
-                let name = attribute "name" heightVar
-                name `shouldBe` ["ETOPRDH_01"]
-                let content' = heightVar $/ content
-                content' `shouldBe` ["Low product height"]
+                case parseTestXml sampleXml of
+                    Left err -> expectationFailure err
+                    Right doc -> do
+                        let xmlInterp = XmlInterp 
+                                { xmlRoot = doc
+                                , dueDiligence = parseDueDiligence doc
+                                , productHeights = parseProductHeights doc
+                                , orderDict = parseOrderDict doc defaultDDOptions
+                                , tableType = parseTableType doc
+                                }
+                        
+                        elecDoc <- createElectricalXml xmlInterp conn
+                        let cursor = fromDocument elecDoc
+                        let vars = cursor $// element "ConfigurationVariable"
+                        length vars `shouldBe` 1
+                        
+                        let heightVar = head vars
+                        attribute "name" heightVar `shouldBe` ["ETOPRDH_01"]
+                        (heightVar $/ content) `shouldBe` ["Low product height"]
+                        
+    describe "Error Handling" $ do
+        it "handles invalid XML gracefully" $ do
+            let invalidXml = "not valid xml"
+            case parseTestXml (TL.fromStrict invalidXml) of
+                Left _ -> return () -- Expected to fail
+                Right _ -> expectationFailure "Should have failed on invalid XML"
 
-    describe "Full Integration" $ do
-        it "should process XML file end-to-end" $ do
+        it "handles database errors gracefully" $ do
             bracket setupTestDb close $ \conn -> do
-                writeFile "test.xml" (TL.unpack sampleXml)
-                
-                result <- parseXmlFile "test.xml"
-                case result of
-                    Left err -> fail err
-                    Right xmlInterp -> do
-                        generateMechanicalXml xmlInterp "test.mech.xml"
-                        mechExists <- doesFileExist "test.mech.xml"
-                        mechExists `shouldBe` True
-                        
-                        generateElectricalXml xmlInterp conn "test.elec.xml"
-                        elecExists <- doesFileExist "test.elec.xml"
-                        elecExists `shouldBe` True
-                        
-                        removeFile "test.xml"
-                        removeFile "test.mech.xml"
-                        removeFile "test.elec.xml"
+                -- Query a non-existent table
+                result <- DB.queryDb conn "nonexistent_table" "param" "value"
+                result `shouldBe` Nothing
